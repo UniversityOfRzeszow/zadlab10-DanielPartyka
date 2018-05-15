5d7b66efbcd47d98b8744b7be18218a800f00058
Joanna Z <johanna.95@o2.pl>
2018-05-15 17:24:47 +0200

    4 zadanko z lab 10

---
+++ OOPL10/src/main/java/pl/edu/ur/oopl10/Dzielenie.java
@@ -0,0 +1,18 @@
+
+package lab10;
+
+
+public class Dzielenie 
+{
+  public static void main(String[] args)
+{
+    int a=10,b=0;
+try {
+    
+    double dzielenie = a/b;
+} catch (ArithmeticException e)
+{
+    System.out.println("Nie mozna dzielic przez 0");
+}
+}
+}
